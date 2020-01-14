f47d075ac2541fe8cffa09593f3ad72acdcd8d55
ivanmanoel96 <ivanmanoel96@hotmail.com>
2020-01-14 15:49:27 -0300

    Add files via upload

---
+++ uISubstitui.pas
@@ -0,0 +1,15 @@
+unit uISubstitui;
+
+interface
+
+type
+
+  ISubstitui = interface
+    function Substituir(aStr, aVelho, aNovo: String): String;
+  end;
+
+implementation
+
+end.
+
+
---
+++ uSubstitui.pas
@@ -0,0 +1,68 @@
+unit uSubstitui;
+
+interface
+
+uses
+  uISubstitui;
+
+type
+
+  TSubstitui = class(TInterfacedObject, ISubstitui)
+  public
+    function Substituir(aStr, aVelho, aNovo: String): String;
+  end;
+
+implementation
+
+function TSubstitui.Substituir(aStr, aVelho, aNovo: String): String;  
+
+  //Devolve parte da string a partir da posição inicial até a final
+  function CortaString(pStr: String; pPosInicial, pPosFinal: Integer): String;
+  var
+    aPosicao: Integer;
+  begin
+    Result := '';
+    for aPosicao := pPosInicial to pPosFinal do
+    begin
+      Result := Result + pStr[aPosicao];  
+    end; 
+  end;
+
+  //Devolve a string substituindo o valor de aVelho contido em pStr pelo aNovo
+  function Substitui(pStr: String): String;
+  var
+    aPosicao,
+    aQntCaracteres: Integer;
+    aResultado: String;
+  begin
+    aQntCaracteres := 0;
+    aResultado := '';
+    for aPosicao := 1 to Length(pStr) do
+    begin
+      if pStr[aPosicao] = aVelho[aQntCaracteres+1] then
+        aQntCaracteres := aQntCaracteres + 1
+      else begin
+        aQntCaracteres := 0;
+
+        if pStr[aPosicao] = aVelho[1] then
+          aQntCaracteres := 1;
+      end;
+
+      if aQntCaracteres = Length(aVelho) then begin
+        aResultado := CortaString(pStr, 1, aPosicao - aQntCaracteres) + aNovo;
+        Result := aResultado + Substitui(CortaString(pStr, aPosicao +1, Length(pStr)));
+        Exit;
+      end;
+    end;
+
+    if aResultado = '' then
+      Result := pStr;
+  end; 
+  
+begin
+  Result := Substitui(aStr);
+end;
+
+end.
+
+
