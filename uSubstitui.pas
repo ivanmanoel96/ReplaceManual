9b97030c8763a4f564177f4112bf1c0a65b54e31
Ivan Silva <ivanmanoel96@hotmail.com>
2020-01-16 03:22:46 -0300

    Add files via upload

--- uSubstitui.pas
+++ uSubstitui.pas
@@ -14,18 +14,20 @@ type
 
 implementation
 
-function TSubstitui.Substituir(aStr, aVelho, aNovo: String): String;  
+function TSubstitui.Substituir(aStr, aVelho, aNovo: String): String;
 
   //Devolve parte da string a partir da posição inicial até a final
   function CortaString(pStr: String; pPosInicial, pPosFinal: Integer): String;
   var
+    aResultado: String;
     aPosicao: Integer;
   begin
-    Result := '';
+    aResultado := '';
     for aPosicao := pPosInicial to pPosFinal do
     begin
-      Result := Result + pStr[aPosicao];  
-    end; 
+      aResultado := aResultado + pStr[aPosicao];
+    end;
+    Result := aResultado;
   end;
 
   //Devolve a string substituindo o valor de aVelho contido em pStr pelo aNovo
@@ -38,28 +40,23 @@ function TSubstitui.Substituir(aStr, aVelho, aNovo: String): String;
     aQntCaracteres := 0;
     aResultado := '';
 
-    if Length(pStr) > 0 then begin
-      if Length(aVelho) > 0 then
-        for aPosicao := 1 to Length(pStr) do
-        begin
-          if pStr[aPosicao] = aVelho[aQntCaracteres+1] then
-            aQntCaracteres := aQntCaracteres + 1
-          else begin
-            aQntCaracteres := 0;
+    for aPosicao := 1 to Length(pStr) do
+    begin
+      if pStr[aPosicao] = aVelho[aQntCaracteres+1] then
+        aQntCaracteres := aQntCaracteres + 1
+      else begin
+        aQntCaracteres := 0;
 
-            if pStr[aPosicao] = aVelho[1] then
-              aQntCaracteres := 1;
-          end;
+        if pStr[aPosicao] = aVelho[1] then
+          aQntCaracteres := 1;
+      end;
 
-          if aQntCaracteres = Length(aVelho) then begin
-            aResultado := CortaString(pStr, 1, aPosicao - aQntCaracteres) + aNovo;
-            Result := aResultado + Substitui(CortaString(pStr, aPosicao +1, Length(pStr)));
-            Exit;
-          end;
-        end
-    end else
-      if Length(aVelho) = 0 then
-        aResultado := aNovo;
+      if aQntCaracteres = Length(aVelho) then begin
+        aResultado := CortaString(pStr, 1, aPosicao - aQntCaracteres) + aNovo;
+        aResultado := aResultado + Substitui(CortaString(pStr, aPosicao +1, Length(pStr)));
+        Break;
+      end;
+    end;
 
     if aResultado = '' then
       aResultado := pStr;
